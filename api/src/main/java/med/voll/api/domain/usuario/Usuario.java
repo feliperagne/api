9a2196de8bcf290019f23b0fc5a27bfaa83a0d23
Felipe Ragne <frsilveira01@outlook.com>
2023-05-10 10:13:55 -0300

    login funcionando

--- api/src/main/java/med/voll/api/domain/usuario/Usuario.java
+++ api/src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -5,6 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Table(name = "usuarios")
 @Entity(name = "Usuario")
@@ -12,7 +18,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +26,38 @@ public class Usuario {
     private String login;
     private String senha;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER")); //VOU TER APENAS O ROLE DO TIPO DE USUARIO; CASO TIVESSE MAIS TIPOS DE USUARIO (COMO CLIENTE, FUNCIONARIO, ETC), FARIA MAIS ALGUNS ROLES;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;  //FAZER O SPRING ENTENDER QUE ESSE ATRIBUTO É A SENHA
+    }
+
+    @Override
+    public String getUsername() {
+        return login; //FAZER O SPRING ENTENDER QUE ESSE ATRIBUTO É O LOGIN
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
